eaa0cf9ff6df02dc5526405b0cfabfdd996e339f
Hoang Son Tung <megatunger@gmail.com>
2019-03-22 03:24:09 +0700

    CameraView & Captured Photos in base64

--- Dev_Team/iOS/BlindVision/BlindVision/Resources/ViewControllers/FirstViewController.swift
+++ Dev_Team/iOS/BlindVision/BlindVision/Resources/ViewControllers/FirstViewController.swift
@@ -7,14 +7,105 @@
 //
 
 import UIKit
+import AVFoundation
 
-class FirstViewController: UIViewController {
+let urlString = "http://52.163.230.167:5000/v1/api/predict"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class FirstViewController: UIViewController, AVCapturePhotoCaptureDelegate {
+    
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    
+    struct JSONBody: Codable {
+        let image : String
+        let name : String
+    }
+    //When user tap on camera
+    @IBAction func didTakePhoto(_ sender: Any) {
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    //Handle the captured photo
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        guard let imageData = photo.fileDataRepresentation()
+            else { return }
+
+        // @image: AVCapture --> UI Image
+        let image = UIImage(data: imageData)
+    
+        /*
+        *** Convert image to base64
+        */
+        
+        guard let imgData = image?.jpegData(compressionQuality: 0.75) else {
+            return()
+        }
+//        print(imgData.base64EncodedString())
+        
+        guard let url = URL(string: urlString) else {return}
+        URLSession.shared.dataTask(with: url) {
+            (data, response,  error) in
+            if  error != nil {
+                print(error!.localizedDescription)
+            }
+        }
+    }
+    
+    func setupLivePreview() {
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        previewView.layer.addSublayer(videoPreviewLayer)
+        
+        DispatchQueue.global(qos: .userInitiated).async { //[weak self] in
+            self.captureSession.startRunning()
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.previewView.bounds
+            }
+        }
+    }
+    
+    func pushCameraToController() {
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .hd1920x1080
+        
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else {
+                print("Unable to access back camera!")
+                return
+        }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            stillImageOutput = AVCapturePhotoOutput()
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error  {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        pushCameraToController()
+        
     }
 
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
 }
 
