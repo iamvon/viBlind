ed615584cf2d2057200e53a526bc3bc10a681714
Hoang Son Tung <megatunger@gmail.com>
2019-03-21 23:29:30 +0700

    Getting Started Done

---
+++ Dev_Team/iOS/BlindVision/BlindVision/Resources/ViewControllers/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BlindVision
+//
+//  Created by Hoàng Sơn Tùng on 3/21/19.
+//  Copyright © 2019 Hoàng Sơn Tùng. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
